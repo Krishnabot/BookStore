507730e8edef38dea03a536fa24964907b155cd9
Krishna <libraryismyparadise@gmail.com>
2022-08-25 11:58:11 +0545

    Update README.md

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
-const store = configureStore({
+const bookStore = configureStore({
   reducer: {
     bookReducer,
     categoryReducer,
   },
 });
 
-export default store;
+export default bookStore;
