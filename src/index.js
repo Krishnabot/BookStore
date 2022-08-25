bb4e71a3b69019ae15d4a2be390c28b83a7e8f58
Krishna <libraryismyparadise@gmail.com>
2022-08-25 23:20:40 +0545

    Import Redux Provider and book store in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import bookStore from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={bookStore}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
