90ce1983f496da811d5c0927ee78e01c5192fe79
Krishna <libraryismyparadise@gmail.com>
2022-08-26 10:39:36 +0545

    Consume API

---
+++ src/redux/api/consumeAPI.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const bookUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/D4PCunlqO0IhsVdMGExZ/books';
+const LOADING = 'Loading';
+const fetchBook = createAsyncThunk(LOADING, async () => {
+  const response = await axios.get(fetchBook);
+  const { data } = response;
+  const Data = Object.keys(data).map((key) => ({
+    id: key,
+    ...data[key][0],
+  }));
+  return Data;
+});
+
+export { fetchBook, bookUrl };
